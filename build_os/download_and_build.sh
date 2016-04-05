13c79b4bdb2f6aba80b114b164ab727d380dfe88
bboortz <benjamin.boortz@web.de>
2016-04-05 22:32:13 +0200

    download_and_build.sh added /BB

---
+++ build_os/download_and_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# bash <( curl https://raw.githubusercontent.com/bboortz/archlinux-scripts/master/build_os/build.sh )
+#
+
+set -e
+set -i
+set -x
+
+
+. .config
+
+
+pacman -Sy git
+git clone https://github.com/bboortz/archlinux-scripts.git
+cd archlinux-scripts/build_os
+./build.sh
+
