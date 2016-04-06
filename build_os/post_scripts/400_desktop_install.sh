48747dd1924a8a9c60488a76db4d29f73e87ffed
bboortz <benjamin.boortz@web.de>
2016-04-06 06:17:54 +0200

    xfce install fixed /BB

--- build_os/post_scripts/400_desktop_install.sh
+++ build_os/post_scripts/400_desktop_install.sh
@@ -1,6 +1,8 @@
 echo "*** install desktop environment  ***"
-pacman -S xfce4 xfce4-goodies human-icon-theme --needed
-pacman -S xorg-xdm xdm-archlinux xorg-server xorg-xinit xorg-utils xorg-server-utils --needed
-pacman -S networkmanager gnome-keyring network-manager-applet nm-connection-editor
+pacman -Sy --noconfirm xfce4 xfce4-goodies human-icon-theme --needed
+pacman -Sy --noconfirm xorg-xdm xdm-archlinux xorg-server xorg-xinit xorg-utils xorg-server-utils --needed
+pacman -Sy --noconfirm networkmanager gnome-keyring network-manager-applet nm-connection-editor
+pacman -Sy --noconfirm networkmanager gnome-keyring network-manager-applet nm-connection-editor
+pacman -Sy --noconfirm xf86-video-vesa
 systemctl enable xdm-archlinux
 
