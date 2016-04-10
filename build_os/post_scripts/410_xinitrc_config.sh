1726068042b10d09d1b8d9672ccec667d34b2418
Benjamin Boortz <benjamin.boortz@web.de>
2016-04-10 09:52:13 +0200

    set language for X11/lxde /BB

--- build_os/post_scripts/410_xinitrc_config.sh
+++ build_os/post_scripts/410_xinitrc_config.sh
@@ -1,3 +1,10 @@
 echo "*** config xinitrc ***"
-echo "exec startxlxde" > /home/${USER_DESKTOP}/.xinitrc
+
+cat <<-END > /home/${USER_DESKTOP}/.xinitrc
+export LC_ALL=de_DE.UTF-8
+export LANGUAGE=de_DE.UTF-8
+export LANG=de_DE.UTF-8
+exec startxlxde
+END
+
 chown ${USER_DESKTOP}:${USER_DESKTOP} /home/${USER_DESKTOP}/.xinitrc
