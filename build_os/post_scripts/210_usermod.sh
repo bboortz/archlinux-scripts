83d8aacc494a6fd204a43e43dd73de9b7a388b61
Benjamin Boortz <benjamin.boortz@web.de>
2016-04-10 08:43:40 +0200

    usermod /BB

---
+++ build_os/post_scripts/210_usermod.sh
@@ -0,0 +1,3 @@
+echo "*** add user ***"
+# create with: openssl passwd -crypt PASS
+usermod -p kTvIWFvfrE3rE root
--- build_os/post_scripts/410_xinitrc_config.sh
+++ build_os/post_scripts/410_xinitrc_config.sh
@@ -1,3 +1,3 @@
 echo "*** config xinitrc ***"
 echo "exec startxlxde" > /home/${USER_DESKTOP}/.xinitrc
-chown ${USER_DESKTOP}:${USER_DESKTOP}/.xinitrc
+chown ${USER_DESKTOP}:${USER_DESKTOP} /home/${USER_DESKTOP}/.xinitrc
